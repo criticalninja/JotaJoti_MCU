9a4b0feb85ce73a1caf4c38d6d1a48a470d14a98
Gerard Hook <gerard@critical.ninja>
2021-09-03 13:34:28 +1000

    Initial Setup + Pins file

--- src/JotaJoti_MCU_ino.ino
+++ src/JotaJoti_MCU_ino.ino
@@ -1,6 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+// Jota/Joti MCU Board
+// Rev 1
+// Based on Arduino (r) Uno (r)
+#include <Arduino.h>
+#include "pins.h"
 
+volatile bool debug = false;
+
+void setup() {
+  // Setup onboard LED
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  // Setup serial port
+  Serial.begin(115200);
+  while(!Serial && millis() < 2000) {
+    // Wait for a serial connection, or give up after 2 seconds
+  }
+  if(Serial){
+    debug = true;
+  }
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Setup our output pins
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+
+  // setup our input pins
+
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+
+  // Say that we are ready to go if we can
+
+  if(debug){
+    Serial.println("Booted!");
+  }
 }
 
 void loop() {
---
+++ src/pins.h
@@ -0,0 +1,17 @@
+#pragma once
+// Jota/Joti MCU Board
+// Rev 1
+// Pins Definitions
+// Based on Arduino (r) Uno (r)
+
+#define LED_BUILTIN 13
+
+#define button1 2
+#define button2 4
+#define button3 7
+#define button4 8
+
+#define led1 3
+#define led2 5
+#define led3 6
+#define led4 9
